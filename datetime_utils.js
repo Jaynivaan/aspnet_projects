0da6e0d3526a9ab028c5f5ab84607cbb1d9daa39
helloworld <86576481+Jaynivaan@users.noreply.github.com>
2026-02-28 16:19:54 -0500

    Delete datetime_utils.js

--- datetime_utils.js
+++
@@ -1,7 +0,0 @@
-// Function to print the current date and time
-function printCurrentDatetime() {
-  const datetime = new Date();
-  console.log(`Current Date and Time: ${datetime.toISOString()}`);
-}
-
-module.exports = { printCurrentDatetime };
